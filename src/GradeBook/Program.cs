79252e10604da472c7b53dbf38110542f040fc98
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-08-31 11:20:31 -0300

    method

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,6 +9,7 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book();
+            book.AddGrade(9.1);
 
             var grades = new List<double>() {12.7, 20.3, 5.5};
             grades.Add(21.5);
